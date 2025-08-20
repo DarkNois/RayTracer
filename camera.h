3de4333c7c2133f30c0e0c45ccd54d422ec976a8
DarkNois <gabrieldosreisrabelo@gmail.com>
2025-08-20 16:05:12 -0300

    AntiAliasing implementado

--- camera.h
+++ camera.h
@@ -13,6 +13,7 @@ public:
 
     double aspect_ratio = 1.0; // ratio of image width over height
     int image_width = 100; //rendered image width in pixel count
+    int samples_per_pixel = 10;
     
     void render (const hittable &world)
     {
@@ -25,12 +26,13 @@ public:
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; i++)
             {
-                auto pixel_center = pixel00_loc + ( i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-                color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0,0,0);
+                for (int sample = 0; sample < samples_per_pixel; sample++)
+                {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, world);
+                }
+                write_color(std::cout, pixel_samples_scale * pixel_color);
             }
         }
 
@@ -41,6 +43,7 @@ private:
     /* private camera variables here */
     
     int image_height; // rendered image height
+    double pixel_samples_scale; //color scale factor for a sum of pixel samples
     point3 center; //camera center
     point3 pixel00_loc; //location of pixel 0, 0
     vec3 pixel_delta_u; //offset to pixel to the right
@@ -50,6 +53,8 @@ private:
     {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_samples_scale = 1.0 / samples_per_pixel;
 
         center = point3(0, 0, 0);
 
@@ -69,6 +74,24 @@ private:
         //calculate the location of the upper left pixel.
         auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    }
+
+    ray get_ray (int i, int j) const
+    {
+        // construct a camera ray originating grom the origin and directed at randomly sampled
+        // point around the pixel location i, j
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() const
+    {
+        //returns the vector to a random point in the [-0.5, -0.5]-[+0.5, +0.5] unit square.
+        return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
     color ray_color(const ray &r, const hittable &world)
--- rtweekend.h
+++ rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <random>
 #include <cstdlib>
 #include <iostream>
 #include <limits>
@@ -25,8 +26,9 @@ inline double degrees_to_radians(double degrees)
 
 inline double random_double()
 {
-    //returns a random real in [0,1]
-    return std::rand() / (RAND_MAX + 1.0);
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
 }
 
 inline double random_double(double min, double max)
