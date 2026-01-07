ff6057c1615c6c8c9dcdaf94f0aaab2fa2eb95ed
DarkNois <gabrieldosreisrabelo@gmail.com>
2026-01-07 08:12:50 -0300

    Configuração do GitKraken

---
+++ rtweekend.h
@@ -0,0 +1,47 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <random>
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+//C++ Std Usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+//constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+//utility functions
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max)
+{
+    //returns a random real in [min,man]
+    return min + (max-min)*random_double();
+}
+
+//commom headers
+
+#include "color.h"
+#include "interval.h"
+#include "ray.h"
+#include "vec3.h"
+
+#endif
